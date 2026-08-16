97ade516d80b4a2225f9f252ba12bf8152affd88
danjue79 <daniel@juenck.de>
2026-08-16 21:47:09 +0200

    v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "flexicarb-v3";
+const CACHE = "flexicarb-v5";
 const FILES = ["./", "./index.html", "./manifest.json",
                "./icon-180.png", "./icon-192.png", "./icon-512.png"];
 
